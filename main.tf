27f6bb2dd218cdeda2f12effcc76e023c3a5c519
avmang <mavoyan@griddynamics.com>
2025-05-13 17:01:08 +0400

    removed storage module from main

--- main.tf
+++ main.tf
@@ -25,9 +25,9 @@ module "network" {
   region     = var.region
 }
 
-module "storage" {
-  source = "./modules/storage"
-  # project_id = var.project_id
-  # region = var.region
-}
+# module "storage" {
+#   source = "./modules/storage"
+#   # project_id = var.project_id
+#   # region = var.region
+# }
 
