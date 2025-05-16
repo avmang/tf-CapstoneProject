6af2003ca29ceed9bab31df3082325d8cae01fd4
avmang <mavoyan@griddynamics.com>
2025-05-16 15:10:58 +0400

    fixed gar repo name

--- modules/iam/google_project_iam_binding.tf
+++ modules/iam/google_project_iam_binding.tf
@@ -1,7 +1,7 @@
 resource "google_artifact_registry_repository_iam_binding" "binding" {
   project = var.project_id
   location = var.region
-  repository = var.gar_repo
+  repository = var.gar_repo.name
   role = "roles/artifactregistry.reader"
   members = [
     "serviceAccount:${google_service_account.k8s_gar_access.account_id}@${var.project_id}.iam.gserviceaccount.com"
